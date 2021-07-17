489e004a0bfef6d8bcd36b7d879a0b845c26a7d1
Aman Chourasia <76176138+amanchourasiayt@users.noreply.github.com>
2021-07-17 22:29:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -----------------------------
-# About the Project: This is a Code to Print Green Coloured Test Passed with Tickmark Message!
+# About the Project: This is a Code written in Python to Print Green Coloured Test Passed with Tickmark Message!
 # Author: Aman Chourasia
 # Website: www.amanchourasia.in
 # Date of Creation: 17th July 2021
